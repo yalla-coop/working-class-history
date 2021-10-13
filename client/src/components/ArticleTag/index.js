13dc4bb054b536583663f7555b1ce5082803a815
fadi <fadeomar2015@gmail.com>
2021-10-13 13:18:39 +0300

    create ArticleTag component

---
+++ client/src/components/ArticleTag/index.js
@@ -0,0 +1,26 @@
+import * as S from './style';
+import * as T from '../Typography';
+
+const Shape = ({ shape, shapeColor }) => {
+  if (shape === 'circle') {
+    return <S.Circle shapeColor={shapeColor} />;
+  }
+  if (shape === 'triangle') {
+    return <S.Triangle shapeColor={shapeColor} />;
+  }
+  return <S.Square shapeColor={shapeColor} />;
+};
+
+const ArticleTag = ({ shape, shapeColor, label, value }) => {
+  return (
+    <S.Wrapper>
+      <Shape shape={shape} shapeColor={shapeColor} />
+      <T.H4 color="neutral" weight="bold" ml="5">
+        {label}
+      </T.H4>
+      <T.P ml="4">{value}</T.P>
+    </S.Wrapper>
+  );
+};
+
+export default ArticleTag;
