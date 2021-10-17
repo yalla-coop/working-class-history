82f51186b3b9c33ce7683dd39354ed018b8132c4
fadi <fadeomar2015@gmail.com>
2021-10-17 22:39:14 +0300

    export the components form index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,5 +6,22 @@ import GoBack from './GoBack';
 import * as Typography from './Typography';
 import * as Inputs from './Inputs';
 import * as Icon from './Icon';
+import Image from './Image';
+import TextSection from './TextSection';
+import Card from './Card';
+import ArticlesSection from './ArticlesSection';
 
-export { Button, Grid, Layout, Route, GoBack, Typography, Inputs, Icon };
+export {
+  Button,
+  Grid,
+  Layout,
+  Route,
+  GoBack,
+  Typography,
+  Inputs,
+  Icon,
+  TextSection,
+  Image,
+  Card,
+  ArticlesSection,
+};
