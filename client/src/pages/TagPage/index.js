768c3eb17441c8e8959a754a1b03b96affce0372
fadi <fadeomar2015@gmail.com>
2021-10-19 17:28:40 +0300

    add load moar functionality

--- client/src/pages/TagPage/index.js
+++ client/src/pages/TagPage/index.js
@@ -1,3 +1,5 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import {
   Typography as T,
   Grid,
@@ -5,12 +7,31 @@ import {
   Card,
   ArticlesSection,
 } from '../../components';
-import { latestDummyData, articles } from './dummy-data';
-import { useParams } from 'react-router-dom';
+import { getTagData } from '../../api-calls/Tag';
+import * as S from './style';
 
 const { Col, Row } = Grid;
 
+const Articles = ({ articles }) => {
+  const artLength = articles.length % 6;
+  const artSections = [];
+  for (let i = 0; i < artLength; i++) {
+    let items = articles.slice(i * 6, i * 6 + 6);
+    artSections.push(items);
+  }
+  return artSections.map(
+    (item) => !!item.length && <ArticlesSection articles={item} />
+  );
+};
+
 const TagPage = () => {
+  const [data, setData] = useState({});
+  const [showItems, setShowItems] = useState(10);
+  useEffect(() => {
+    const tagData = getTagData();
+    setData(tagData);
+  }, []);
+
   const { tagName } = useParams();
   return (
     <>
@@ -18,22 +39,27 @@ const TagPage = () => {
       <Row>
         <Col w={[4, 6, 6]} mt="8">
           <Card bgColor="tertiaryMain">
-            <T.H4 color="white">Description of the tag here</T.H4>
+            <T.H4 color="white">{data.subtitle}</T.H4>
             <T.P color="white" weight="light" mt="3">
-              In December 1925, a three-day general strike broke out in
-              Arequipa, Peru, against "Conscripción Vial": forced conscription
-              to road building for anyone who could not afford to pay a fee to
-              get out of it...
+              {data.description}
             </T.P>
           </Card>
         </Col>
-        {latestDummyData.map((item, i) => (
-          <Col w={[4, 6, 6]} mt="8" jc={i % 2 ? 'flex-start' : 'flex-end'}>
-            <TextSection key={item.id} {...item} />
-          </Col>
-        ))}
+        {data.articles &&
+          data.articles.slice(0, 5).map((item, i) => (
+            <Col w={[4, 6, 6]} mt="8" jc={i % 2 ? 'flex-start' : 'flex-end'}>
+              <TextSection key={item.id} {...item} />
+            </Col>
+          ))}
       </Row>
-      <ArticlesSection articles={articles} />
+      {data.articles && (
+        <Articles articles={data.articles.slice(5, showItems)} />
+      )}
+      {data.articles && data.articles.length > showItems && (
+        <S.LoadMore onClick={() => setShowItems((old) => old + 6)}>
+          <T.P underline>Load more...</T.P>
+        </S.LoadMore>
+      )}
     </>
   );
 };
---
+++ client/src/pages/TagPage/style.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const LoadMore = styled.button`
+  outline: none;
+  border: none;
+  background: none;
+  margin-top: ${({ theme }) => theme.spacings[6]};
+  cursor: pointer;
+`;
