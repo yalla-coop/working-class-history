6667c45b2627df442716a79ae32c09bbb5cb6f9b
fadi <fadeomar2015@gmail.com>
2021-10-20 01:11:49 +0300

    create index page

---
+++ client/src/pages/IndexPage/index.js
@@ -0,0 +1,60 @@
+import { Typography as T, Grid, Tags } from '../../components';
+import * as S from './style';
+import { GENERAL } from '../../constants/nav-routes';
+
+const { Col, Row } = Grid;
+const { Category } = Tags;
+
+const IndexPage = () => {
+  return (
+    <>
+      <T.H1>Index</T.H1>
+      <Row>
+        <Col w={[4, 12, 8]}>
+          <T.P mt="6" color="neutral">
+            Lorem ipsum some information about how you can click each tag below
+            to discover articles for that specifc
+          </T.P>
+        </Col>
+      </Row>
+      <S.CategoriesWrapper mt="6">
+        <Category
+          shape="square"
+          shapeColor="primaryMain"
+          title="Topics"
+          to={GENERAL.CATEGORY.replace(':categoryName', 'topics')}
+        />
+        <Category
+          shape="circle"
+          shapeColor="tertiaryMain"
+          title="People"
+          to={GENERAL.CATEGORY.replace(':categoryName', 'people')}
+          mt="7"
+        />
+        <Category
+          shape="triangle"
+          shapeColor="neutral"
+          title="Organisations"
+          to={GENERAL.CATEGORY.replace(':categoryName', 'organisations')}
+          mt="7"
+        />
+        <Category
+          shape="square"
+          shapeColor="primaryMain"
+          title="City/areas"
+          to={GENERAL.CATEGORY.replace(':categoryName', 'areas')}
+          mt="7"
+        />
+        <Category
+          shape="circle"
+          shapeColor="tertiaryMain"
+          title="Countries"
+          to={GENERAL.CATEGORY.replace(':categoryName', 'countries')}
+          mt="7"
+        />
+      </S.CategoriesWrapper>
+    </>
+  );
+};
+
+export default IndexPage;
