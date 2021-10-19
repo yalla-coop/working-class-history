2b76994f537d72e7f60c8c8261b42c04aa60ce89
fadi <fadeomar2015@gmail.com>
2021-10-20 01:12:31 +0300

    update tagList component

--- client/src/components/Tags/TagList/index.js
+++ client/src/components/Tags/TagList/index.js
@@ -1,13 +1,20 @@
 import * as S from './style';
 import * as T from '../../Typography';
+import { GENERAL } from '../../../constants/nav-routes';
 
-const TagList = ({ tagsList = [] }) => {
+const TagList = ({ tagsList = [], ...props }) => {
   return (
-    <S.Wrapper>
+    <S.Wrapper {...props}>
       {tagsList.map((tag, index) => (
         <>
-          <S.TagLink key={tag.to} to={tag.to}>
-            <T.P>{tag.title}</T.P>
+          <S.TagLink
+            key={tag.id}
+            to={GENERAL.TAG.replace(':tagId', tag.id).replace(
+              ':tagName',
+              tag.name
+            )}
+          >
+            <T.P>{tag.label}</T.P>
           </S.TagLink>
           {index !== tagsList.length - 1 && <S.Edge />}
         </>
