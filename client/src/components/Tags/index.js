d2ba647b4bfb30df9122459f376e7ea9bae47d70
fadi <fadeomar2015@gmail.com>
2021-10-20 19:16:38 +0300

    export Reson component from Tags folder

--- client/src/components/Tags/index.js
+++ client/src/components/Tags/index.js
@@ -1,5 +1,6 @@
 import Category from './Category';
 import ArticleTag from './ArticleTag';
 import TagList from './TagList';
+import Reason from './Reason';
 
-export { Category, ArticleTag, TagList };
+export { Category, ArticleTag, TagList, Reason };
