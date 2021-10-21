75a6c07b9e6eafb86af8a5912adb2b1dc19daade
fadi <fadeomar2015@gmail.com>
2021-10-21 12:15:35 +0300

    create submit success page

---
+++ client/src/pages/SubmitEventPage/index.js
@@ -0,0 +1,36 @@
+import * as S from './style';
+import { Typography as T } from '../../components';
+import { GENERAL } from '../../constants/nav-routes';
+
+const SubmitEventPage = () => {
+  return (
+    <S.Wrapper>
+      <T.H1 mt="0" mtM="6" color="white">
+        Amazing!
+      </T.H1>
+      <T.P mt="9" mtM="5" color="white" style={{ maxWidth: 470 }}>
+        Thanks for contributing. We’ll give your article and review. Our article
+        curation is run by volunteers but we’ll do our best to email you with
+        confirmation once approved.
+      </T.P>
+      <S.ButtonWrapper>
+        <S.StyledButton bgColor="neutral" to={GENERAL.LandingPage}>
+          <T.P size="extraLarge" weight="semi" color="primaryMain">
+            Return
+          </T.P>
+          <T.P
+            size="extraLarge"
+            weight="semi"
+            color="primaryMain"
+            mt="2"
+            mtM="0"
+          >
+            home
+          </T.P>
+        </S.StyledButton>
+      </S.ButtonWrapper>
+    </S.Wrapper>
+  );
+};
+
+export default SubmitEventPage;
