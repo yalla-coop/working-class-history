f4c427a15e25826cb0377242c9b78a9771efa3d2
fadi <fadeomar2015@gmail.com>
2021-10-25 15:41:20 +0300

    update Article page and connected to the backend

--- client/src/pages/ArticlePage/index.js
+++ client/src/pages/ArticlePage/index.js
@@ -9,148 +9,219 @@ import {
   Button,
   TimelineGraphic,
 } from '../../components';
+import { Skeleton } from 'antd';
 
-import { getArticleData } from '../../api-calls/Article';
+import { getArticleById, getTags } from '../../api-calls/content';
 
 import SocialSection from './SocialSection';
 import { useParams } from 'react-router';
+import { GENERAL } from '../../constants/nav-routes';
 
 const { Col, Row } = Grid;
 const { ArticleTag, Category } = Tags;
 
+const filterTags = (relatedTags, allTags, category) => {
+  if (relatedTags && relatedTags.length && allTags && allTags.length) {
+    const _relatedTags = relatedTags?.map((item) => item.id) || [];
+    return allTags.filter(
+      (item) =>
+        _relatedTags.includes(item.id) && category === item.Category.value
+    );
+  }
+  return [];
+};
+
 const ArticlePage = () => {
   const [data, setData] = useState({});
-  const { articleName } = useParams();
+  const [tags, setTags] = useState([]);
+  const [pageError, setPageError] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const { articleName, id } = useParams();
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const { error, data } = await getArticleById({ id });
+      setData(data);
+      setLoading(false);
+      if (error) {
+      }
+    } catch (error) {
+      setPageError(error.message);
+    }
+  };
+
+  const getAllTags = async () => {
+    try {
+      setLoading(true);
+      const { error, data } = await getTags();
+      setTags(data.results);
+      setLoading(false);
+      if (error) {
+      }
+    } catch (error) {
+      setPageError(error.message);
+    }
+  };
+
   useEffect(() => {
-    const articleData = getArticleData();
-    setData(articleData);
+    getData();
+    getAllTags();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <>
-      <T.H2>{articleName}</T.H2>
-      <ArticleTag
-        shape="square"
-        shapeColor="primaryMain"
-        label="Date"
-        value={data.date}
-        mt="30px"
-      />
-      <S.ArticleContent>
-        <SocialSection url={window.location.href} />
-
-        <Image image={data.image} mt="36px" mtT="2" />
-        <T.P mt="5" mb="2">
-          {data.imageDescription}
+      <T.H2>{data?.title || articleName}</T.H2>
+      {pageError && (
+        <T.P color="error" mt="8">
+          {pageError}
         </T.P>
+      )}
+      {loading ? (
+        <Skeleton key="skeleton" loading={loading} active></Skeleton>
+      ) : (
+        <>
+          <ArticleTag
+            shape="square"
+            shapeColor="primaryMain"
+            label="Date"
+            value={data?.created_at}
+            mt="30px"
+          />
+          <S.ArticleContent>
+            <SocialSection url={window.location.href} />
 
-        <Row mt="9">
-          <Col w={[4, 10, 10]}>
-            <T.P>{data.articleContent}</T.P>
-            <T.P mt="5">{data.articleContent1}</T.P>
-          </Col>
-        </Row>
-      </S.ArticleContent>
-      <S.MapSection mt="11" mb="3">
-        <Image image="map" width="100%" />
-      </S.MapSection>
-      <MoreInformation
-        mt="9"
-        mb="2"
-        subtitle={data.infoTitle}
-        link={data.infoLink}
-      />
-      <MoreInformation
-        mt="9"
-        mb="8"
-        title="Sources"
-        subtitle={data.sourceTitle}
-        link={data.sourceLink}
-      />
-      <ArticleTag
-        shape="triangle"
-        shapeColor="primaryMain"
-        label="Author"
-        value={data.author}
-        mt="5"
-      />
-      <ArticleTag
-        shape="circle"
-        shapeColor="primaryMain"
-        label="Fact checker"
-        value={data.checker}
-        mt="5"
-      />
-      <ArticleTag
-        shape="Last edited"
-        shapeColor="primaryMain"
-        label="Date"
-        value={data.lastEdited}
-        mt="5"
-      />
-      <T.H3 mt="9">Related tags</T.H3>
-      <Row>
-        <Col w={[4, 12, 6]} mt="6">
-          <Category
-            shape="square"
-            shapeColor="primaryMain"
-            size="small"
-            title="People"
-            to="/people"
-            relatedTags={data.peopleTags}
+            <Image src={data?.media} mt="36px" mtT="2" />
+            <T.P mt="5" mb="2">
+              {data?.media_caption}
+            </T.P>
+
+            <Row mt="9">
+              <Col w={[4, 10, 10]}>
+                <T.P>{data?.description}</T.P>
+                {/* <T.P mt="5">{data.articleContent1}</T.P> */}
+              </Col>
+            </Row>
+          </S.ArticleContent>
+          <S.MapSection mt="11" mb="3">
+            <Image image="map" width="100%" />
+          </S.MapSection>
+          <MoreInformation
+            mt="9"
+            mb="2"
+            subtitle="CLR James, A History of Pan African Revolt,"
+            link="https://shop.workingclasshistory.com/products/a-history-of-pan-african-revolt-c-l-r-james"
           />
-        </Col>
-        <Col w={[4, 12, 6]} mt="6">
-          <Category
-            shape="square"
-            shapeColor="primaryMain"
-            size="small"
-            title="City/Area"
-            to="/area"
-            relatedTags={data.areaTags}
-          />
-        </Col>
-        <Col w={[4, 12, 6]} mt="6">
-          <Category
-            shape="circle"
-            shapeColor="tertiaryMain"
-            size="small"
-            title="Topic"
-            to="/topic"
-            relatedTags={data.topicTags}
-          />
-        </Col>
-        <Col w={[4, 12, 6]} mt="6">
-          <Category
-            shape="circle"
-            shapeColor="tertiaryMain"
-            size="small"
-            title="Country"
-            to="/country"
-            relatedTags={data.countryTags}
-          />
-        </Col>
-        <Col w={[4, 12, 6]} mt="6">
-          <Category
+          {data?.sources && (
+            <MoreInformation
+              mt="9"
+              mb="8"
+              title="Sources"
+              subtitle={data.sources
+                .split(' ')
+                .filter((str) => !str.startsWith('http'))}
+              link={data.sources
+                .split(' ')
+                .find((str) => str.startsWith('http'))}
+            />
+          )}
+          <ArticleTag
             shape="triangle"
-            size="small"
-            shapeColor="neutral"
-            title="Organisation"
-            to="/organisations"
-            relatedTags={data.organisationTags}
+            shapeColor="primaryMain"
+            label="Author"
+            value={data?.author_name || 'N/A'}
+            mt="5"
           />
-        </Col>
-      </Row>
-      <Row mt="9">
-        <Col w={[4, 7, 7]}>
-          <Button
-            bgColor="neutral"
-            textColor="primaryMain"
-            text="Support us"
-            to="/support-us"
+          <ArticleTag
+            shape="circle"
+            shapeColor="primaryMain"
+            label="Fact checker"
+            value={data?.reviwer_id?.value || 'N/A'}
+            mt="5"
           />
-        </Col>
-      </Row>
-      <TimelineGraphic type="short" number={data.year} />
+          <ArticleTag
+            shape="Last edited"
+            shapeColor="primaryMain"
+            label="Date"
+            value={data?.created_at || 'N/A'}
+            mt="5"
+          />
+          <T.H3 mt="9">Related tags</T.H3>
+          <Row>
+            <Col w={[4, 12, 6]} mt="6">
+              <Category
+                shape="square"
+                shapeColor="primaryMain"
+                size="small"
+                title="People"
+                to={GENERAL.CATEGORY.replace(':categoryName', 'people')}
+                relatedTags={filterTags(data.articles_to_tags, tags, 'person')}
+              />
+            </Col>
+            <Col w={[4, 12, 6]} mt="6">
+              <Category
+                shape="square"
+                shapeColor="primaryMain"
+                size="small"
+                title="City/Area"
+                to={GENERAL.CATEGORY.replace(':categoryName', 'area')}
+                relatedTags={filterTags(
+                  data.articles_to_tags,
+                  tags,
+                  'city_area'
+                )}
+              />
+            </Col>
+            <Col w={[4, 12, 6]} mt="6">
+              <Category
+                shape="circle"
+                shapeColor="tertiaryMain"
+                size="small"
+                title="Topic"
+                to={GENERAL.CATEGORY.replace(':categoryName', 'topic')}
+                relatedTags={filterTags(data.articles_to_tags, tags, 'topic')}
+              />
+            </Col>
+            <Col w={[4, 12, 6]} mt="6">
+              <Category
+                shape="circle"
+                shapeColor="tertiaryMain"
+                size="small"
+                title="Country"
+                to={GENERAL.CATEGORY.replace(':categoryName', 'country')}
+                relatedTags={filterTags(data.articles_to_tags, tags, 'country')}
+              />
+            </Col>
+            <Col w={[4, 12, 6]} mt="6">
+              <Category
+                shape="triangle"
+                size="small"
+                shapeColor="neutral"
+                title="Organisation"
+                to={GENERAL.CATEGORY.replace(':categoryName', 'organisation')}
+                relatedTags={filterTags(
+                  data.articles_to_tags,
+                  tags,
+                  'organisation'
+                )}
+              />
+            </Col>
+          </Row>
+          <Row mt="9">
+            <Col w={[4, 7, 7]}>
+              <Button
+                bgColor="neutral"
+                textColor="primaryMain"
+                text="Support us"
+                to={GENERAL.CONTRIBUTE}
+              />
+            </Col>
+          </Row>
+        </>
+      )}
+      <TimelineGraphic type="short" number={data?.year} />
     </>
   );
 };
