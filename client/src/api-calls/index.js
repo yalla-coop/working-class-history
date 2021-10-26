34773f1da2b32d7ec248ba6e0ebfc27e1ac8c365
fadi <fadeomar2015@gmail.com>
2021-10-26 19:55:57 +0300

    remove unused code

--- client/src/api-calls/index.js
+++ client/src/api-calls/index.js
@@ -1,14 +1,7 @@
-import axios from 'axios';
-
 import * as Users from './users';
 import * as Content from './content';
 
 // DUMMY TO BE REPLACED
 import * as Article from './Article';
-
-axios.defaults.baseURL = `https://api.baserow.io/api/database`;
-axios.defaults.headers.common[
-  'Authorization'
-] = `${process.env.REACT_APP_BASEROW_TOKEN}`;
 
 export { Users, Content, Article };
