ee95d3f9518acc231b9ba74b73f6f4ebb362d0e7
fadi <fadeomar2015@gmail.com>
2021-11-02 12:33:49 +0200

    create helper to generate unique id

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,5 +1,6 @@
 import setMargin from './set-margin';
 import cleanEmail from './cleanEmail';
 import ScrollToTop from './ScrollToTop';
+import uniqueID from './unique-id';
 
-export { setMargin, cleanEmail, ScrollToTop };
+export { setMargin, cleanEmail, ScrollToTop, uniqueID };
