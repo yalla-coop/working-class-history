521b106f662de0e313dd07b8ef1947397ecd1a5c
fadi <fadeomar2015@gmail.com>
2021-11-02 18:09:45 +0200

    wrap the protected routers by auth provider

--- client/src/App.js
+++ client/src/App.js
@@ -16,55 +16,54 @@ function App() {
     <div className="app">
       <Global styles={globalStyle} />
       <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <Router basename={process.env.PUBLIC_URL}>
-            <ScrollToTop />
-            <Switch>
-              <Route
-                exact
-                path={navRoutes.GENERAL.STORIES}
-                Component={Pages.LandingPage}
-                layout="general"
-                landingPage
-                maxWidth="1050px"
-              />
-              <Route
-                exact
-                path={navRoutes.GENERAL.LandingPage}
-                Component={Pages.LandingPage}
-                layout="general"
-                landingPage
-                maxWidth="1050px"
-              />
-              <Route
-                exact
-                path={navRoutes.GENERAL.TAG}
-                Component={Pages.TagPage}
-                layout="general"
-                maxWidth="1050px"
-              />
-              <Route
-                exact
-                path={navRoutes.GENERAL.INDEX}
-                Component={Pages.IndexPage}
-                layout="general"
-                maxWidth="1050px"
-              />
-              <Route
-                exact
-                path={navRoutes.GENERAL.CATEGORY}
-                Component={Pages.CategoryPage}
-                layout="general"
-                maxWidth="1050px"
-              />
-              <Route
-                exact
-                path={navRoutes.GENERAL.ARTICLE}
-                Component={Pages.ArticlePage}
-                layout="general"
-                maxWidth="1050px"
-              />
-
+        <Router basename={process.env.PUBLIC_URL}>
+          <ScrollToTop />
+          <Switch>
+            <Route
+              exact
+              path={navRoutes.GENERAL.STORIES}
+              Component={Pages.LandingPage}
+              layout="general"
+              landingPage
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.LandingPage}
+              Component={Pages.LandingPage}
+              layout="general"
+              landingPage
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.TAG}
+              Component={Pages.TagPage}
+              layout="general"
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.INDEX}
+              Component={Pages.IndexPage}
+              layout="general"
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.CATEGORY}
+              Component={Pages.CategoryPage}
+              layout="general"
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.ARTICLE}
+              Component={Pages.ArticlePage}
+              layout="general"
+              maxWidth="1050px"
+            />
+            <AuthProvider>
               <Route
                 exact
                 path={navRoutes.ADMIN.AWAITING_REVIEW}
@@ -73,20 +72,7 @@ function App() {
                 maxWidth="1050px"
                 isPrivet
               />
-              <Route
-                exact
-                path={navRoutes.GENERAL.SIGN_UP}
-                Component={Pages.SignupPage}
-                layout="general"
-                maxWidth="1050px"
-              />
-              <Route
-                exact
-                path={navRoutes.GENERAL.CONTRIBUTE}
-                Component={Pages.ContributePage}
-                layout="general"
-                maxWidth="1050px"
-              />
+
               <Route
                 exact
                 path={navRoutes.ADMIN.PENDING_ARTICLE}
@@ -107,20 +93,6 @@ function App() {
 
               <Route
                 exact
-                path={navRoutes.GENERAL.LOGIN}
-                Component={Pages.LoginPage}
-                layout="general"
-                maxWidth="1050px"
-              />
-              <Route
-                exact
-                path={navRoutes.GENERAL.SUCCESS_EVENT_SUBMIT}
-                Component={Pages.SubmitEventPage}
-                layout="message"
-                maxWidth="1050px"
-              />
-              <Route
-                exact
                 path={navRoutes.ADMIN.APPROVED}
                 Component={Pages.ApprovedPage}
                 layout="message"
@@ -135,16 +107,44 @@ function App() {
                 maxWidth="1050px"
                 isPrivet
               />
-              <Route
-                exact
-                path={navRoutes.GENERAL.SUCCESS_SIGN_UP}
-                Component={Pages.SuccessSignupPage}
-                layout="message"
-                maxWidth="1050px"
-              />
-            </Switch>
-          </Router>
-        </AuthProvider>
+            </AuthProvider>
+            <Route
+              exact
+              path={navRoutes.GENERAL.SIGN_UP}
+              Component={Pages.SignupPage}
+              layout="general"
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.CONTRIBUTE}
+              Component={Pages.ContributePage}
+              layout="general"
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.LOGIN}
+              Component={Pages.LoginPage}
+              layout="general"
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.SUCCESS_EVENT_SUBMIT}
+              Component={Pages.SubmitEventPage}
+              layout="message"
+              maxWidth="1050px"
+            />
+            <Route
+              exact
+              path={navRoutes.GENERAL.SUCCESS_SIGN_UP}
+              Component={Pages.SuccessSignupPage}
+              layout="message"
+              maxWidth="1050px"
+            />
+          </Switch>
+        </Router>
       </ThemeProvider>
     </div>
   );
