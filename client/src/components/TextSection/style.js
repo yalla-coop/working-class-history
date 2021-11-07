23e3c402c922be5d46032c4642d2d9cc1d511435
fadi <fadeomar2015@gmail.com>
2021-11-07 12:07:22 +0200

    render rich text

--- client/src/components/TextSection/style.js
+++ client/src/components/TextSection/style.js
@@ -7,4 +7,10 @@ export const Wrapper = styled.div`
   max-width: 423px;
 `;
 
+export const Div = styled.div`
+  max-height: 245px;
+  overflow: hidden;
+  width: 100%;
+`;
+
 export const ReadMore = styled(Link)``;
