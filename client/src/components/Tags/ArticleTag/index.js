b7a05087afdbe368db2b47454da98e7071cb8ec7
fadi <fadeomar2015@gmail.com>
2021-11-07 12:06:43 +0200

    show author link if its provided on tags

--- client/src/components/Tags/ArticleTag/index.js
+++ client/src/components/Tags/ArticleTag/index.js
@@ -11,7 +11,14 @@ const Shape = ({ shape, shapeColor }) => {
   return <S.Square shapeColor={shapeColor} />;
 };
 
-const ArticleTag = ({ shape, shapeColor, label, value, ...props }) => {
+const ArticleTag = ({
+  shape,
+  shapeColor,
+  label,
+  value,
+  children,
+  ...props
+}) => {
   return (
     <S.Wrapper {...props}>
       <Shape shape={shape} shapeColor={shapeColor} />
@@ -19,9 +26,12 @@ const ArticleTag = ({ shape, shapeColor, label, value, ...props }) => {
         <T.H4 color="neutral" weight="bold" ml="5">
           {label}
         </T.H4>
-        <T.P ml="4" mlM="5">
-          {value}
-        </T.P>
+        {value && (
+          <T.P ml="4" mlM="5">
+            {value}
+          </T.P>
+        )}
+        {children}
       </S.ContentWrapper>
     </S.Wrapper>
   );
